2cbdd52dc051315ccee98fcb025b62ac4cb07468
uknodaisuki <fkeita@kuhp.kyoto-u.ac.jp>
2018-04-05 11:12:05 +0900

    hajimete

---
+++ src/PositionFix_.java
@@ -0,0 +1,11 @@
+import ij.IJ;
+import ij.plugin.PlugIn;
+
+public class PositionFix_ implements PlugIn{
+	public void run (String arg) {
+		String plg_ver = "0.0";
+		IJ.log("starting PosFx plugin ver" +plg_ver);
+
+
+	}
+}
